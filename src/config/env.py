1d38266ab4987bd7a19bc93e961992b65f10df9d
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2025-06-09 21:29:14 -0600

    Fix env vars

--- src/config/env.py
+++ src/config/env.py
@@ -25,24 +25,23 @@ def load_env_vars():
     # Set defaults in case they're not defined, where appropriate
     # Handle type casting here, instead of throughout the code
 
-    # DEBUG INFO WARNING ERROR CRITICAL
-    env_vars["LOG_LEVEL"]                               = str(environ.get("LOG_LEVEL"                               , "INFO" ))
-    env_vars["MAX_CONCURRENT_CONVERSIONS_TOTAL"]        = int(environ.get("MAX_CONCURRENT_CONVERSIONS_TOTAL"        , 10     ))
-    env_vars["MAX_CONCURRENT_CONVERSIONS_PER_SERVER"]   = int(environ.get("MAX_CONCURRENT_CONVERSIONS_PER_SERVER"   , 10     ))
-    env_vars["MAX_CYCLES"]                              = int(environ.get("MAX_CYCLES"                              , 0      ))
-    env_vars["MAX_RETRIES"]                             = int(environ.get("MAX_RETRIES"                             , 3      ))
-    env_vars["REPO_CONVERTER_INTERVAL_SECONDS"]         = int(environ.get("REPO_CONVERTER_INTERVAL_SECONDS"         , 3600   ))
-    # Path inside the container to find this file, only change to match if the right side of the volume mapping changes
-    env_vars["REPOS_TO_CONVERT"]                        = str(environ.get("REPOS_TO_CONVERT"                        , "/sourcegraph/repos-to-convert.yaml" ))
-    # Path inside the container to find this directory, only change to match if the right side of the volume mapping changes
-    env_vars["SRC_SERVE_ROOT"]                          = str(environ.get("SRC_SERVE_ROOT"                          , "/sourcegraph/src-serve-root" ))
-
-    # Image build info
+    # Build metadata
     env_vars["BUILD_BRANCH"]                            = str(environ.get("BUILD_BRANCH"                            , "" ))
     env_vars["BUILD_COMMIT"]                            = str(environ.get("BUILD_COMMIT"                            , "" ))
     env_vars["BUILD_DATE"]                              = str(environ.get("BUILD_DATE"                              , "" ))
     env_vars["BUILD_DIRTY"]                             = str(environ.get("BUILD_DIRTY"                             , "" ))
     env_vars["BUILD_TAG"]                               = str(environ.get("BUILD_TAG"                               , "" ))
+    # DEBUG INFO WARNING ERROR CRITICAL
+    env_vars["LOG_LEVEL"]                               = str(environ.get("LOG_LEVEL"                               , "INFO" ))
+    env_vars["MAX_CONCURRENT_CONVERSIONS_PER_SERVER"]   = int(environ.get("MAX_CONCURRENT_CONVERSIONS_PER_SERVER"   , 10     ))
+    env_vars["MAX_CONCURRENT_CONVERSIONS_TOTAL"]        = int(environ.get("MAX_CONCURRENT_CONVERSIONS_TOTAL"        , 10     ))
+    # Max cycles of the main loop, then the container exits
+    env_vars["MAX_CYCLES"]                              = int(environ.get("MAX_CYCLES"                              , 0      ))
+    env_vars["MAX_RETRIES"]                             = int(environ.get("MAX_RETRIES"                             , 3      ))
+    env_vars["REPO_CONVERTER_INTERVAL_SECONDS"]         = int(environ.get("REPO_CONVERTER_INTERVAL_SECONDS"         , 3600   ))
+    # Paths inside the container, don't change unless also changing in compose file volume mapping
+    env_vars["REPOS_TO_CONVERT"]                        = str(environ.get("REPOS_TO_CONVERT"                        , "/sourcegraph/repos-to-convert.yaml" ))
+    env_vars["SRC_SERVE_ROOT"]                          = str(environ.get("SRC_SERVE_ROOT"                          , "/sourcegraph/src-serve-root" ))
 
 
 # def load_config_from_repos_to_convert_file():
