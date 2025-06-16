af9c169b149db54296eeab001a084beac3e65dd3
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2025-06-16 17:35:36 -0600

    Increase build output

--- build/build.sh
+++ build/build.sh
@@ -4,13 +4,16 @@
 ## Usage:
 
 # ./build.sh
-# Run the build, do not start the containers
-
-# ./build.sh c
-# Run the build, start the containers, clear the terminal, and follow repo-converter's logs
+# Just run the build, do not start the containers
 
 # ./build.sh <any>
-# Run the build, start the containers, and follow repo-converter's logs
+# Run the build, start the containers
+
+# ./build.sh cf
+# Run the build, start the containers, clear the terminal, and follow repo-converter's logs
+
+# ./build.sh pcf
+# Run the build, pull newer containers, start the containers, clear the terminal, and follow repo-converter's logs
 
 
 # TODO: check if the GitHub Actions build script can't be reused here?
@@ -39,17 +42,20 @@ set -x
 set -o errexit
 
 
-# Update the requirements.txt file
-# APPLICATION_CODE_DIR="../src/"
-# pipreqs --force --mode no-pin --savepath "$REQUIREMENTS_FILE" "$APPLICATION_CODE_DIR"
+# pip requirements
+req_file="./requirements.txt"
+echo "Update and deduplicate the ${req_file} file"
 
-# Sort and deduplicate the ${REQUIREMENTS_FILE} file
-REQUIREMENTS_FILE="./requirements.txt"
-echo "Sort and deduplicate the ${REQUIREMENTS_FILE} file"
-LC_ALL=C sort -u -o "$REQUIREMENTS_FILE" "$REQUIREMENTS_FILE"
+# Update the requirements.txt file
+# src_dir="../src/"
+# pipreqs --force --mode no-pin --savepath "$req_file" "$src_dir"
+
+# Sort and deduplicate the ${req_file} file
+LC_ALL=C sort -u -o "$req_file" "$req_file"
 
 
 # Set environment variables from git metadata of this repo
+echo "Gathering environment variables to bake into image's .env file"
 BUILD_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 BUILD_COMMIT="$(git rev-parse --short HEAD)"
 BUILD_DATE="$(date -u +'%Y-%m-%d %H:%M:%S UTC')"
@@ -69,34 +75,40 @@ ENV_FILE=".env"
 } > "$ENV_FILE"
 
 # Run the build
+echo "Running the podman-compose build"
 podman-compose build repo-converter
 
 # If you pass any args to this script, start the built image, and follow the logs
-if [ "$1" != "" ]
+if [[ "$1" != "" ]]
 then
 
     # Stop any running containers
     # because podman-compose can't figure this out on its own
+    echo "Stopping old containers"
     podman-compose down
 
     # Pull the latest tags of the other images
-    if [ "$1" == "*p*" ]
+    if [[ "$1" == *"p"* ]]
     then
-        podman-compose pull
+        echo "Checking for new images and pulling"
+        podman-compose pull cloud-agent src-serve-git
     fi
 
     # Start the compose deployment
+    echo "Starting new containers"
     podman-compose up -d --remove-orphans
 
     # Clear the terminal
-    if [ "$1" == "*c*" ]
+    if [[ "$1" == *"c"* ]]
     then
+        echo "Clearing terminal"
         clear
     fi
 
     # Follow the container logs
-    if [ "$1" == "*f*" ]
+    if [[ "$1" == *"f"* ]]
     then
+        echo "Following repo-converter logs"
         podman-compose logs repo-converter -f
     fi
 
