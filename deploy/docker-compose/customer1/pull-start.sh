e95419a05617e841b7fded20ef0573599e27f844
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2025-07-14 18:48:23 -0600

    Update script

--- deploy/docker-compose/customer1/pull-start.sh
+++ deploy/docker-compose/customer1/pull-start.sh
@@ -8,6 +8,15 @@
 # crontab -e
 # */30 * * * * bash /sg/repo-converter/deploy/docker-compose/customer1/pull-start.sh
 
+## Get script args
+
+# If an f is passed into the script args, then try to fix the ownership and permissions of files in the src-serve-git directory
+if [[ "$1" == *"f"* ]]
+then
+    fix_perms="true"
+else
+    fix_perms="false"
+fi
 
 ## Setup
 # Define file paths
@@ -20,6 +29,7 @@ docker_compose_full_file_path="$repo_dir/$docker_compose_dir/$docker_compose_fil
 docker_cmd="docker compose -f $docker_compose_full_file_path"
 docker_up_sleep_seconds=10
 git_cmd="git -C $repo_dir"
+git_branch_cmd="$git_cmd branch -vv"
 log_file="/var/log/sg/pull-start.log"
 src_serve_root_dir="/sg/src-serve-root"
 
@@ -49,58 +59,63 @@ log "Verifying file/dir ownerships in $src_serve_root_dir"
 count_of_files_not_owned_by_me=$(find "$src_serve_root_dir" \! -user "$CURRENT_USER" -printf 1 | wc -c)
 log "Found $count_of_files_not_owned_by_me files/dirs in $src_serve_root_dir not owned by current user"
 
-
-# If an f is passed in the args
-if [[ "$1" == *"m"* ]]
+if [[ "$fix_perms" == "true" ]]
 then
-
     log "Attempting to take ownership of these files/dirs"
     find "$src_serve_root_dir" \! -user "$CURRENT_USER" -exec chown "$CURRENT_UID_GID" {} \;
-
+else
+    log "Skipping attempt to take ownership of these files/dirs"
 fi
+
 
 log "Verifying file permissions in $src_serve_root_dir"
 
 count_of_files_with_wrong_perms=$(find "$src_serve_root_dir" -type f \! -perm 644 -printf 1 | wc -c)
 log "Found $count_of_files_with_wrong_perms files $src_serve_root_dir with incorrect permissions"
 
-
-# If an f is passed in the args
-if [[ "$1" == *"m"* ]]
+if [[ "$fix_perms" == "true" ]]
 then
-
     log "Attempting to fix permissions on these files"
     find "$src_serve_root_dir" -type f \! -perm 644 -exec chmod 644 {} \;
-
+else
+    log "Skipping attempt to fix permissions on these files"
 fi
+
 
 log "Verifying directory permissions in $src_serve_root_dir"
 
 count_of_dirs_with_wrong_perms=$(find "$src_serve_root_dir" -type d \! -perm 755 -printf 1 | wc -c)
 log "Found $count_of_dirs_with_wrong_perms directories $src_serve_root_dir with incorrect permissions"
 
-
-# If an f is passed in the args
-if [[ "$1" == *"m"* ]]
+if [[ "$fix_perms" == "true" ]]
 then
-
     log "Attempting to fix permissions on these directories"
     find "$src_serve_root_dir" -type d \! -perm 755 -exec chmod 755 {} \;
-
+else
+    log "Skipping attempt to fix permissions on these directories"
 fi
 
 ## Run Git and Docker commands
-log "On branch before git pull: $($git_cmd branch -v)"
+
+log "Pruning remote git branches deleted from remote"
+git fetch --prune
+
+log "Pruning local git branches deleted from remote"
+# Command from https://stackoverflow.com/a/17029936
+git branch --remotes | awk '{print $1}' | grep -E -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
+
+log "On branch before git pull:"
+$git_branch_cmd
 
 log "Docker compose file: $docker_compose_full_file_path"
 log "docker ps before:"
 $docker_cmd ps
 
 command="\
-    $git_cmd reset --hard                                                && \
-    $git_cmd pull --force                                                && \
-    $docker_cmd pull                                                     && \
-    CURRENT_UID_GID=$CURRENT_UID_GID $docker_cmd up -d --remove-orphans     \
+    $git_cmd reset --hard                                                &&\
+    $git_cmd pull --force                                                &&\
+    $docker_cmd pull                                                     &&\
+    CURRENT_UID_GID=$CURRENT_UID_GID $docker_cmd up -d --remove-orphans    \
     "
 
 log "Running command in a sub shell:"
@@ -110,7 +125,8 @@ echo "$command" | awk 'BEGIN{FS="&&"; OFS="&& \n"} {$1=$1} 1'
 # Run the command
 bash -c "$command" >> "$log_file" 2>&1
 
-log "On branch after git pull: $($git_cmd branch -v)"
+log "On branch after git pull:"
+$git_branch_cmd
 
 log "Sleeping $docker_up_sleep_seconds seconds to give Docker containers time to start and stabilize"
 sleep $docker_up_sleep_seconds
